c4048f182ef2429b195ee8ea756dbaad726761c4
chubby <thegamer92letsgo@gmail.com>
2025-09-13 22:13:27 -0400

    changing bc for ish

--- bc.sh
+++ bc.sh
@@ -5,5 +5,6 @@ echo "
 echo "this is a script that installs BC for you"
 echo "do you wish to continue? (ctrl+c= no   passowrd= yes) bc is required to run CAL"
 sudo apt install bc
+apk add bc
 bash cal2.sh
 clear
