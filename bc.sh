38bb4879d158671ba868f46e914051c5adb4d21d
chubby <thegamer92letsgo@gmail.com>
2025-09-13 22:15:23 -0400

    Update bc
    deleted because it doesn't work

--- bc.sh
+++ bc.sh
@@ -5,6 +5,5 @@ echo "
 echo "this is a script that installs BC for you"
 echo "do you wish to continue? (ctrl+c= no   passowrd= yes) bc is required to run CAL"
 sudo apt install bc
-apk add bc
 bash cal2.sh
 clear
