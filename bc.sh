4434337964b6ce8424c1d17b062408e9705def45
m4sk3d <thegamer92letsgo@gmail.com>
2026-08-19 17:43:45 -0400

    Enhance bc installation script with package manager support
    Updated installation script to support multiple package managers and added success message.

--- bc.sh
+++ bc.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 echo "
-█▀▀ ▄▀█ █░░
-█▄▄ █▀█ █▄▄"
+█▀▀ ▄▀█ █░░
+█▄▄ █▀█ █▄▄"
 echo "this is a script that installs BC for you"
-echo "do you wish to continue? (ctrl+c= no   passowrd= yes) bc is required to run CAL"
-sudo apt install bc
-bash cal2.sh
-clear
+echo "do you wish to continue? (ctrl+c= no   password= yes) bc is required to run CAL"
+
+if command -v apt &> /dev/null; then
+    sudo apt update && sudo apt install -y bc
+elif command -v pacman &> /dev/null; then
+    sudo pacman -S --noconfirm bc
+elif command -v dnf &> /dev/null; then
+    sudo dnf install -y bc
+elif command -v brew &> /dev/null; then
+    brew install bc
+else
+    echo "Could not find a supported package manager (apt, pacman, dnf, brew)."
+    echo "Please install 'bc' manually!"
+    exit 1
+fi
+
+echo "bc installed successfully!"
